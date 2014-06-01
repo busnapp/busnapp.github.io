4af9d6164bec4fba93768760d12f9c6035728615
Lahiru K. Rasnayake <devilledchicken@gmail.com>
2014-06-01 22:40:04 +0530

    added geolocation

--- a3.js
+++ a3.js
@@ -9,26 +9,63 @@ $scope.hdn=name;
 var map;
 function initialize() {
   var mapOptions = {
-    zoom: 12,
+    zoom: 15,
     center: new google.maps.LatLng(7.1,79.9428)
   };
   map = new google.maps.Map(document.getElementById('map-canvas'),
       mapOptions);
+      if(navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(function(position) {
+      var pos = new google.maps.LatLng(position.coords.latitude,
+                                       position.coords.longitude);
+
+      var infowindow = new google.maps.InfoWindow({
+        map: map,
+        position: pos,
+        content: 'You are here!'
+      });
+
+      map.setCenter(pos);
+    }, function() {
+      handleNoGeolocation(true);
+    });
+  } else {
+    // Browser doesn't support Geolocation
+    
+  }
+   
+    
+}
+
+
+
+function handleNoGeolocation(errorFlag) {
+  if (errorFlag) {
+    var content = 'Error: The Geolocation service failed.';
+  } else {
+    var content = 'Error: Your browser doesn\'t support geolocation.';
+  }
+
+  var options = {
+    map: map,
+    position: new google.maps.LatLng(7.9,79.9428),
+    content: content
+  };
+
+  var infowindow = new google.maps.InfoWindow(options);
+  map.setCenter(options.position);
 }
 
  
 function drpmnu($scope){
-$scope.places=['Nugegoda','Koswatta','Malabe'];
+$scope.places=['Nugegoda','Koswatta','Malabe','Nawala',
+'Rajagiriya','Battaramulla'];
 
 }
 
-function testid($scope){
-    $scope.placed=document.getElementById('To').value;
-}
 
 
-function txtbx(){
-    var indd=document.getElementById('To').value;
-  var outdd=document.getElementById('tbox');
-    outdd.value=indd;
-    }
+
+
+
+
